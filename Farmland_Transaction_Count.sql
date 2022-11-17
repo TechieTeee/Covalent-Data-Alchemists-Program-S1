d6fa6124af287d6c20bc4c1cf75c7c3d783b976c
TechieTeee <100870737+TechieTeee@users.noreply.github.com>
2022-11-16 22:06:11 -0600

    Update and rename Farmland_Transaction_Count.sql to Vending_Machines_RRR.sql

--- Farmland_Transaction_Count.sql
+++
@@ -1,8 +0,0 @@
-SELECT  [signed_at:aggregation] as date
-        , uniq(tx_hash) as transactions                                                              
-FROM blockchains.all_chains
-WHERE chain_name = 'avalanche_mainnet'
-        AND tx_recipient = unhex('00f5D01D86008D14d04E29EFe88DffC75a9cAc47')
-        AND successful = 1
-        AND [signed_at:daterange]
-GROUP BY date
