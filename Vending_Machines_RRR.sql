cd5ea8b34fd2e9c8135cd5773aec859d74df2e40
TechieTeee <100870737+TechieTeee@users.noreply.github.com>
2022-11-21 23:58:54 -0600

    Update Vending_Machines_RRR.sql

--- Vending_Machines_RRR.sql
+++ Vending_Machines_RRR.sql
@@ -1,1 +1,10 @@
--
+#Active Addresses for Vending Machines NFT
+
+SELECT  [signed_at:aggregation] as date
+        , uniq(tx_sender) AS active_addresses
+FROM blockchains.all_chains 
+WHERE tx_recipient = unhex('BbD9786f178e2AEBb4b4329c41A821921ca05339')
+        AND [signed_at:daterange]
+        AND chain_name = 'avalanche_mainnet'
+GROUP BY date
+ORDER BY date desc
