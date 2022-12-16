1a150663da8903966151dffbc87c9b0486e9a2be
TechieTeee <100870737+TechieTeee@users.noreply.github.com>
2022-12-15 23:14:28 -0600

    Update NFTrade_RRR_Analysis_Reach.sql

--- Week 3/NFTrade_RRR_Analysis_Reach.sql
+++ Week 3/NFTrade_RRR_Analysis_Reach.sql
@@ -17,3 +17,15 @@ and market = 'nftrade'
 and collection_name != ''
 and [signed_at:daterange]
 GROUP BY date, collection_name
+
+
+/*NFTrade Sales By Collection (AVAX)*/
+SELECT  [signed_at:aggregation] as date
+        , count(tx_hash) as NFT_sales,
+        collection_name
+FROM reports.nft_sales_all_chains
+where chain_name = 'avalanche_mainnet'
+and market = 'nftrade'
+and collection_name != ''
+and [signed_at:daterange]
+GROUP BY date, collection_name
