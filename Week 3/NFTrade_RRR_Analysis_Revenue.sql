43ca0db115d50fd01107ba42f47856925d89ae1a
TechieTeee <100870737+TechieTeee@users.noreply.github.com>
2022-12-19 00:13:19 -0600

    Update NFTrade_RRR_Analysis_Revenue.sql

--- Week 3/NFTrade_RRR_Analysis_Revenue.sql
+++ Week 3/NFTrade_RRR_Analysis_Revenue.sql
@@ -75,3 +75,44 @@ GROUP BY date
 SELECT cte2.date,
 cte1.volume/cte2.volume AS sales_marketshare
 FROM cte2 JOIN cte1 ON cte2.date=cte1.date
+
+
+/*NFTrade Revenue/User USDC & Growth Rate*/
+/*Avalanche Main Net*/
+with cte1 as (      SELECT [signed_at:aggregation] as date, uniq(taker) as buyers
+                    FROM reports.nft_sales_all_chains
+                    WHERE chain_name = 'avalanche_mainnet'
+                    AND market = 'nftrade'
+                    AND [signed_at:daterange] 
+                    GROUP BY date
+                    ORDER BY date desc),
+cte2 as (
+SELECT date, volume
+FROM(
+SELECT [signed_at:aggregation] as date
+       ,sum((fees_paid_unscaled/pow(10,18))*price_in_usd) as volume 
+       ,lagInFrame(volume) OVER (ORDER BY date) as previous
+FROM reports.nft_sales_all_chains sales
+  LEFT JOIN (
+    SELECT dt, contract_address, price_in_usd
+    FROM reports.token_prices 
+		WHERE chain_name = 'avalanche_mainnet'
+        AND contract_address = lower('B31f66AA3C1e785363F0875A1B74E27b85FD66c7')
+		) prices
+				ON date_trunc('day', sales.signed_at) = prices.dt
+WHERE chain_name = 'avalanche_mainnet'
+AND market = 'nftrade'
+AND [signed_at:daterange]
+GROUP BY date
+ORDER BY date ASC 
+))
+
+SELECT final_date, volume as revenue_per_buyer, (volume/previous)-1 as growth_rate
+FROM(
+SELECT cte2.date as final_date
+       ,cte2.volume/cte1.buyers as volume 
+       ,lagInFrame(volume) OVER (ORDER BY final_date) as previous
+FROM cte2 JOIN cte1 ON cte1.date=cte2.date
+GROUP BY final_date, volume
+ORDER BY final_date ASC 
+)
